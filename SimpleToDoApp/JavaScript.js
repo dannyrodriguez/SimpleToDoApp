4054b7a02af673f1c928a1354a2f93c6faeeffb7
dannyrodriguez <daniel.rodriguez36@yahoo.com>
2014-04-02 18:29:01 -0500

    Unfin1

--- SimpleToDoApp/JavaScript.js
+++ SimpleToDoApp/JavaScript.js
@@ -1,4 +1,11 @@
 ﻿function clickButton() {
     var text = document.getElementById("todoInput").value;
-    document.getElementById("list").innerHTML += "<li>" + text + "</li>";
+    document.getElementById("list").innerHTML += "<li id = 'rock' onclick='finish();'>" + text + "</li>";
+}
+
+function finish() {
+    li.innerHTML = todoItem;
+    li.setAttribute("id", newID);
+    li.setAttribute("onclick", )
+   
 }
