0d6e1c282da0ddbf2e019490bf3236bfb97840bf
dannyrodriguez <daniel.rodriguez36@yahoo.com>
2014-04-03 08:16:00 -0500

    Cleaner Code Commit

--- SimpleToDoApp/JavaScript.js
+++ SimpleToDoApp/JavaScript.js
@@ -1,17 +1,17 @@
 ﻿var counter = 0;
 
 function clickButton() {
+    "user strict";
+
     var text = document.getElementById("todoInput").value;
-    document.getElementById("list").innerHTML += "<li id = 'rock" + counter + "' onclick=\"finish("+counter+");\">" + text + "</li>";
+    document.getElementById("itemList").innerHTML += "<li id = 'item" + counter + "' onclick=\"finish("+counter+");\">" + text + "</li>";
     counter += 1;
 }
 
 function finish(num1) {
-    var idName = "rock"+num1;
+    "user strict";
+
+    var idName = "item"+num1;
     var sap = document.getElementById(idName);
-    sap.setAttribute("class", "strikethrough");
-    //document.getElementById("rock).setAttribute("class", "strikethrough");
-
-
-   
+    sap.setAttribute("class", "strikethrough"); 
 }
