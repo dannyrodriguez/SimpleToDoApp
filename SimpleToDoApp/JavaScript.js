863af78c8763bc21842f4802ac90327c2175a38f
dannyrodriguez <daniel.rodriguez36@yahoo.com>
2014-04-02 19:27:16 -0500

    Added Strike

--- SimpleToDoApp/JavaScript.js
+++ SimpleToDoApp/JavaScript.js
@@ -1,11 +1,15 @@
-﻿function clickButton() {
+﻿var counter = 0;
+
+function clickButton() {
     var text = document.getElementById("todoInput").value;
-    document.getElementById("list").innerHTML += "<li id = 'rock' onclick='finish();'>" + text + "</li>";
+    document.getElementById("list").innerHTML += "<li id = 'rock" + counter + "' onclick=\"finish("+counter+");\">" + text + "</li>";
+    counter += 1;
 }
 
-function finish() {
-    li.innerHTML = todoItem;
-    li.setAttribute("id", newID);
-    li.setAttribute("onclick", )
+function finish(counter) {
+    var idName = "rock"+counter+"";
+    document.getElementById(idName).setAttribute("style", "text-decoration: line-through;");
+
+
    
 }
